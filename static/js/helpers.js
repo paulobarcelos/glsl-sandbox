3b84e251cdbd3f7daf4e2fcd565b256afccf4cf0
Paulo Barcelos <paulo@azucrina.org>
2012-09-16 00:13:12 +0200

    Added vertex shader editor

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -23,39 +23,78 @@ function load_url_code() {
 
 			// Basic format
 
-			code.value = decodeURIComponent( hash );
+			fragCode.value = decodeURIComponent( hash );
 
 		}
 
 	} else {
 
-		readURL( '5d000001009a0200000000000000119a48c65ab5aec1f910f780dfdfe473e599a211a90304ab6aa581b342b344db4e71099beb79352b3c442c8dee970ffb4d054491e356b4f55882c2f3554393fe6662cf2c348a3f51dcce7b5760290bbc5c1b937d382ba6cdd0a9b35cf7fd57cebd800501c16f80f61ad4501d00a2ca4e63c8dc38b7b03703cba8d68914c6f2c6598f2f7008faee0e4b4cf4276eea6d0fb93df9188dae5b7f6db2579246363efaf9145f13206ee5b908e90eb4f6e19254a0f4fda81b31c2d3fd00e78e5b5fb5d5e51df87412a667211e121d77f3becd58d5960f9b77d8b826d4c6bce27a589f7158944441ae8fa5a297f23f0e7707f84fcbe0557976aaca9c97b99d3252a8b85b2a4ecb10d9b3cb65f6a5d75240f8bde39ed692b559c61276fe260578' );
+		readURL( '5d000001009a0200000000000000119a48c65ab5aec1f910f780dfdfe473e599a211a90304ab6aa581b342b344db4e71099beb79352b3c442c8dee970ffb4d054491e356b4f55882c2f3554393fe6662cf2c348a3f51dcce7b5760290bbc5c1b937d382ba6cdd0a9b35cf7fd57cebd800501c16f80f61ad4501d00a2ca4e63c8dc38b7b03703cba8d68914c6f2c6598f2f7008faee0e4b4cf4276eea6d0fb93df9188dae5b7f6db2579246363efaf9145f13206ee5b908e90eb4f6e19254a0f4fda81b31c2d3fd00e78e5b5fb5d5e51df87412a667211e121d77f3becd58d5960f9b77d8b826d4c6bce27a589f7158944441ae8fa5a297f23f0e7707f84fcbe0557976aaca9c97b99d3252a8b85b2a4ecb10d9b3cb65f6a5d75240f8bde39ed692b559c61276fe260578____5d00000100550000000000000000309d2ce46aacd0ed69f8859a375ab05b4cb4d15f6e13cfcd8ae44532e6e3aa4a0b196f25587fb3b034e68ea54bca6f593f722427dcf38fbca93b9b3414a00f642cfbd0107d7a860fe85fff4f364000' );
 
 	}
 }
 
 function setURL( shaderString ) {
+	shaderString = shaderString.split("____");
+	var fragString = shaderString[0];
+	var vertString = shaderString[1];
 
-	compressor.compress( shaderString, 1, function( bytes ) {
+	var fragHex, vertHex;
+	var fragReady = false;
+	var vertReady = false;
 
-		var hex = convertBytesToHex( bytes );
-		window.location.replace( '#A/' + hex );
+	function replaceLocation(){
+		window.location.replace( '#A/' + fragHex + '____' + vertHex );
+	}
 
+	compressor.compress( fragString, 1, function( bytes ) {
+		fragHex = convertBytesToHex( bytes );
+		fragReady = true;
+		if(fragReady && vertReady) replaceLocation();
+	},
+	dummyFunction );
+
+	compressor.compress( vertString, 1, function( bytes ) {
+		vertHex = convertBytesToHex( bytes );
+		vertReady = true;
+		if(fragReady && vertReady) replaceLocation();
 	},
 	dummyFunction );
 
 }
 
 function readURL( hash ) {
+	hash = hash.split("____");
+	var fragString = hash[0];
+	var vertString = hash[1];
 
-	var bytes = convertHexToBytes( hash );
+	var vertBytes = convertHexToBytes( vertString );
+	var fragBytes = convertHexToBytes( fragString );
 
-	compressor.decompress( bytes, function( text ) {
+	var fragShader, vertShader;
+	var fragReady = false;
+	var vertReady = false;
 
+	function executeCompile(){
 		compileOnChangeCode = false;  // Prevent compile timer start
-		code.setValue(text);
+		vertCode.setValue(vertShader);
+		fragCode.setValue(fragShader);
 		compile();
 		compileOnChangeCode = true;
+	}
+
+	compressor.decompress( fragBytes, function( text ) {
+		fragShader = text;
+		fragReady = true;
+		if(fragReady && vertReady) executeCompile();
+
+	},
+	dummyFunction );
+
+	compressor.decompress( vertBytes, function( text ) {
+		vertShader = text;
+		vertReady = true;
+		if(fragReady && vertReady) executeCompile();
 
 	},
 	dummyFunction );
