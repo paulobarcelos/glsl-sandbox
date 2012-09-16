7e84a7c4f88227631c2fd760f88985fdd4fca9c3
Paulo Barcelos <paulo@azucrina.org>
2012-09-16 21:49:23 +0200

    Started adding support for textures

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -29,35 +29,44 @@ function load_url_code() {
 
 	} else {
 
-		readURL( '5d000001009a0200000000000000119a48c65ab5aec1f910f780dfdfe473e599a211a90304ab6aa581b342b344db4e71099beb79352b3c442c8dee970ffb4d054491e356b4f55882c2f3554393fe6662cf2c348a3f51dcce7b5760290bbc5c1b937d382ba6cdd0a9b35cf7fd57cebd800501c16f80f61ad4501d00a2ca4e63c8dc38b7b03703cba8d68914c6f2c6598f2f7008faee0e4b4cf4276eea6d0fb93df9188dae5b7f6db2579246363efaf9145f13206ee5b908e90eb4f6e19254a0f4fda81b31c2d3fd00e78e5b5fb5d5e51df87412a667211e121d77f3becd58d5960f9b77d8b826d4c6bce27a589f7158944441ae8fa5a297f23f0e7707f84fcbe0557976aaca9c97b99d3252a8b85b2a4ecb10d9b3cb65f6a5d75240f8bde39ed692b559c61276fe260578____5d00000100550000000000000000309d2ce46aacd0ed69f8859a375ab05b4cb4d15f6e13cfcd8ae44532e6e3aa4a0b196f25587fb3b034e68ea54bca6f593f722427dcf38fbca93b9b3414a00f642cfbd0107d7a860fe85fff4f364000' );
+		readURL( '5d00000100790200000000000000119a48c65ab5aec1f910f780dfdfe473e599a211a90304ab6aa581b342b344db4e71099beb79352b3c442c8dee970ffb4d054491e356b4f55882c2f3554393fe6662cf2c348a3f51dcce7b5760290bbc5c1b937d382ba6cdd0a9b35cf7fd57cebd8006f8d0890914e3f7b821ef92b0143a5417e183bc9bc4e260bfa30c897f178ecf9a485282e38ae9fa011dfa46174ccc63100f187684b312d20cc52621e00a82e4c5c5a3938614726cdcc86c25f2681251f217873b5c70e8449cd323a904e2140758fd4c62ab6fed75f9859959a20f5d4d1a804bbb8e6ce8467bf19b063880e7fb9d13a71dba124f6cf74fffd59754466d10b818d5179032b59ee0c764bd7b156188ebffbce97a60____5d00000100580000000000000000309d2ce46aacd0ed69f8859a375ab05b4cb4d15f6e13cfcd8ae44532e6e3aa4a0b196f25587fb3b034e68e3e36bc486e8734975733e2479c606e481b5b4c2d04bb501e9aea32758b26e1044bfe7aba00____5d000001001100000000000000003a194b7813eab51a290c91a72ff881044f0d36a93851ffcf190000' );
 
 	}
 }
 
-function setURL( shaderString ) {
-	shaderString = shaderString.split("____");
-	var fragString = shaderString[0];
-	var vertString = shaderString[1];
+function setURL( string ) {
+	string = string.split("____");
+	var fragString = string[0];
+	var vertString = string[1];
+	var texString = string[2];
 
-	var fragHex, vertHex;
+	var fragHex, vertHex, texHex;
 	var fragReady = false;
 	var vertReady = false;
+	var texReady = false;
 
 	function replaceLocation(){
-		window.location.replace( '#A/' + fragHex + '____' + vertHex );
+		window.location.replace( '#A/' + fragHex + '____' + vertHex + '____' + texHex );
 	}
 
 	compressor.compress( fragString, 1, function( bytes ) {
 		fragHex = convertBytesToHex( bytes );
 		fragReady = true;
-		if(fragReady && vertReady) replaceLocation();
+		if(fragReady && vertReady && texReady) replaceLocation();
 	},
 	dummyFunction );
 
 	compressor.compress( vertString, 1, function( bytes ) {
 		vertHex = convertBytesToHex( bytes );
 		vertReady = true;
-		if(fragReady && vertReady) replaceLocation();
+		if(fragReady && vertReady && texReady) replaceLocation();
+	},
+	dummyFunction );
+
+	compressor.compress( texString, 1, function( bytes ) {
+		texHex = convertBytesToHex( bytes );
+		texReady = true;
+		if(fragReady && vertReady && texReady) replaceLocation();
 	},
 	dummyFunction );
 
@@ -67,18 +76,23 @@ function readURL( hash ) {
 	hash = hash.split("____");
 	var fragString = hash[0];
 	var vertString = hash[1];
+	var texString = hash[2];
 
 	var vertBytes = convertHexToBytes( vertString );
 	var fragBytes = convertHexToBytes( fragString );
+	var texBytes = convertHexToBytes( texString );
 
-	var fragShader, vertShader;
+	var fragShader, vertShader, texUrl;
 	var fragReady = false;
 	var vertReady = false;
+	var texReady = false;
 
-	function executeCompile(){
+	function applySettings(){
 		compileOnChangeCode = false;  // Prevent compile timer start
 		vertCode.setValue(vertShader);
 		fragCode.setValue(fragShader);
+		textureURLInput.value = texUrl;
+		loadTexture(texUrl);
 		compile();
 		compileOnChangeCode = true;
 	}
@@ -86,7 +100,7 @@ function readURL( hash ) {
 	compressor.decompress( fragBytes, function( text ) {
 		fragShader = text;
 		fragReady = true;
-		if(fragReady && vertReady) executeCompile();
+		if(fragReady && vertReady && texReady) applySettings();
 
 	},
 	dummyFunction );
@@ -94,7 +108,15 @@ function readURL( hash ) {
 	compressor.decompress( vertBytes, function( text ) {
 		vertShader = text;
 		vertReady = true;
-		if(fragReady && vertReady) executeCompile();
+		if(fragReady && vertReady && texReady) applySettings();
+
+	},
+	dummyFunction );
+
+	compressor.decompress( texBytes, function( text ) {
+		texUrl = text;
+		texReady = true;
+		if(fragReady && vertReady && texReady) applySettings();
 
 	},
 	dummyFunction );
